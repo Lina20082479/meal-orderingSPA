3390d5ab07125bd6754ce993273cd1e8ab483143
Linan Chen <20082479@mail.wit.ie>
2018-12-07 16:21:11 +0000

    Delete a dish E2E testing

--- cypress/integration/Manage.spec.js
+++ cypress/integration/Manage.spec.js
@@ -1,17 +1,37 @@
 describe('Manage', () => {
     before(function () {
-        cy.visit('/login')
+        cy.visit('http://localhost:8080/login')
         cy.get('#inputEmail').type('root@meal.ie')
         cy.get('#inputPassword').type('123456')
         cy.get('.btn').click()
         cy.url().should('include', '/')
-        
-    })
+        cy.request('http://localhost:3000/dishes/')
+          .its('body')
+          .then( (dishes) => {
+             dishes.forEach( (element) => {
+                cy.request('DELETE',
+                   'http://localhost:3000/dishes/' + element._id)
+             });
+          })
+          cy.fixture('dishes')
+            .then((dishes) => {
+                dishes.forEach((dish) => {
+                  cy.request('POST',
+                     'http://localhost:3000/dishes/', dish )
+                })
+          })
+          
+     }); 
     it('allows a dish to be added', () => {
         cy.get('.btn-success').click()
         cy.get('#modalPrevent___BV_modal_body_ > :nth-child(1) > .col-sm-10 > #input-default').type('fried salty chicken')
         cy.get('#modalPrevent___BV_modal_body_ > :nth-child(2) > .col-sm-10 > #input-default').type('chicken')
         cy.get(':nth-child(3) > .col-sm-10 > #input-default').type(6.5)
         cy.get('#modalPrevent___BV_modal_footer_ > .btn-primary').click()
+    })
+    it('allows a dish to be deleted', () => {
+        cy.get('.col-9').find('tr').should('have.length', 5)
+        cy.get(':nth-child(4) > [aria-colindex="4"] > [width="30px;"]').click()
+        cy.get('.col-9').find('tr').should('have.length', 4)
     })
     })
