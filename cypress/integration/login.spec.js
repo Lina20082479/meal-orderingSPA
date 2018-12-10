fb37056cb823dad8236eb99f4ab67a8d775ae142
Linan Chen <20082479@mail.wit.ie>
2018-12-10 16:07:46 +0000

    updete E2E testing

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,7 +3,7 @@ describe('Login', () => {
         cy.visit('/login')
       })
       it('login in fail', () => {
-        cy.get('#inputEmail').type('123456@wit.ie');
+        cy.get('#inputEmail').type('test@wit.ie');
         cy.get('#inputPassword').type('12345');
         cy.get('.btn').click();
         cy.wait(1000)
@@ -11,11 +11,15 @@ describe('Login', () => {
       })
       it('allows a valid user to sign in', () => {
         cy.visit('/login')
-        cy.get('#inputEmail').type('123456@wit.ie');
+        cy.get('#inputEmail').type('test@meal.ie');
         cy.get('#inputPassword').type('123456');
         cy.get('.btn').click();
         cy.wait(1000)
         cy.url().should('include', '/')
       });
+      it('allows a valid user to log out', () => {
+        cy.get('.ml-auto > .btn').click()
+        cy.url().should('include', '/login')
+      })
 
 });
